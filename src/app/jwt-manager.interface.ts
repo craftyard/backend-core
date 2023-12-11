3be809a200ddf781b658f5dcfb2a53c61e1187e4
Nuradil <yaksh0nti@gmail.com>
2023-12-11 15:47:33 +0500

    Обьявлен и реализован JWTManager

---
+++ src/app/jwt-manager.interface.ts
@@ -0,0 +1,14 @@
+import { Realisable } from 'rilata2/src/domain/realisable';
+import { JWTDecoder } from 'rilata2/src/app/jwt/jwt-decoder.interface';
+import { TokenCreator } from 'rilata2/src/app/jwt/token-creator.interface';
+import { TokenVerifier } from 'rilata2/src/app/jwt/token-verifier.interface';
+import { JWTPayload } from 'workshop-domain/src/subject/domain-data/user/user-authentification.a-params';
+
+export interface JWTManager
+extends JWTDecoder<JWTPayload>, TokenVerifier<JWTPayload>, TokenCreator<JWTPayload> {}
+
+export const JWTManager = {
+  instance(resolver: Realisable): JWTManager {
+    return resolver.getRealisatioin(JWTManager) as JWTManager;
+  },
+};
