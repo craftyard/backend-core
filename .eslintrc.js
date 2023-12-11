cfb743c9f6a724e76858d5514aab3789b4a4ccf7
Nuradil <yaksh0nti@gmail.com>
2023-12-11 15:22:43 +0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# craft-yard-backend
